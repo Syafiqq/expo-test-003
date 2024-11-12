aaa5a1b6bbc5e2864444dd52bf9354504391c90b
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-13 00:18:30 +0700

    cache: add auth cache data source di

--- core/di/container.type.ts
+++ core/di/container.type.ts
@@ -2,11 +2,13 @@ type TypesMap = {
   // datasource/cache
   mmkv: symbol;
   cacheStorage: symbol;
+  authCacheDataSource: symbol;
 };
 
 const Types: TypesMap = {
   mmkv: Symbol.for('MMKV'),
   cacheStorage: Symbol.for('CacheStorage'),
+  authCacheDataSource: Symbol.for('AuthCacheDataSource'),
 }
 
 export {
--- core/di/data/datasource/cache-provider.ts
+++ core/di/data/datasource/cache-provider.ts
@@ -3,10 +3,12 @@ import {TypesMap} from "@/core/di/container.type";
 import {storage} from "@/core/data/datasource/cache/impl/mmkv";
 import {MMKV} from "react-native-mmkv";
 import CacheStorage from "@/core/data/datasource/cache/impl/cache-storage";
+import AuthCacheDataSource from "@/core/data/datasource/cache/interface/auth-cache-data-source";
 
 const configure = (container: Container, types: TypesMap): void => {
   container.bind<MMKV>(types.mmkv).toConstantValue(storage);
   container.bind<CacheStorage>(types.cacheStorage).to(CacheStorage).inSingletonScope();
+  container.bind<AuthCacheDataSource>(types.authCacheDataSource).toService(types.cacheStorage);
 }
 
 export default configure;
