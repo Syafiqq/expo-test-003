60696d943ad06671eff47782e01a19792f89ba18
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-12 14:30:39 +0700

    lib: add inversify

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,10 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-proposal-decorators', {legacy: true}],
+      ['@babel/plugin-transform-class-static-block'],
+      ['@babel/plugin-transform-class-properties', {loose: true}],
     ],
   };
 };
