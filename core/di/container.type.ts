08149f9d302b565e0f1479384093679ed05aa42b
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-13 00:14:31 +0700

    cache: add cache storage di

--- core/di/container.type.ts
+++ core/di/container.type.ts
@@ -1,10 +1,12 @@
 type TypesMap = {
   // datasource/cache
   mmkv: symbol;
+  cacheStorage: symbol;
 };
 
 const Types: TypesMap = {
   mmkv: Symbol.for('MMKV'),
+  cacheStorage: Symbol.for('CacheStorage'),
 }
 
 export {
--- core/di/data/datasource/cache-provider.ts
+++ core/di/data/datasource/cache-provider.ts
@@ -2,9 +2,11 @@ import {Container} from "inversify";
 import {TypesMap} from "@/core/di/container.type";
 import {storage} from "@/core/data/datasource/cache/impl/mmkv";
 import {MMKV} from "react-native-mmkv";
+import CacheStorage from "@/core/data/datasource/cache/impl/cache-storage";
 
 const configure = (container: Container, types: TypesMap): void => {
   container.bind<MMKV>(types.mmkv).toConstantValue(storage);
+  container.bind<CacheStorage>(types.cacheStorage).to(CacheStorage).inSingletonScope();
 }
 
 export default configure;
