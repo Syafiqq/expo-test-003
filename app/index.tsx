3592eebc6ba17f61e06316940a0f2df6006ef744
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-14 16:52:09 +0700

    route: update login

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,18 @@
-import {Stack} from "expo-router";
+import {Redirect, Stack} from "expo-router";
 import SplashScreenLayout from "@/components/page/splash-screen-layout";
+import {useAuth} from "@/core/presentation/store/auth-store";
 
 export default function Index() {
+  const status = useAuth.use.status();
+
+  if (status === 'signOut') {
+    return <Redirect href="/login"/>;
+  }
+
+  if (status === 'signIn') {
+    return <Redirect href="/(main)"/>;
+  }
+
   return (
     <>
       <Stack.Screen
