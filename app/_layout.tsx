33a3d7e821a51f5ffcb18e62608f8db3a8e4c678
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-14 08:28:54 +0700

    react-query: init lib

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,18 @@ import {Stack} from "expo-router";
 import {Provider} from "inversify-react";
 import container from "@/core/di/container";
 import SqliteProvider from "@/core/data/datasource/local/impl/sqlite-provider";
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return (
     <Provider container={() => container}>
       <SqliteProvider>
-        <Stack/>
+        <QueryClientProvider client={queryClient}>
+          <Stack/>
+        </QueryClientProvider>
       </SqliteProvider>
     </Provider>
   )
